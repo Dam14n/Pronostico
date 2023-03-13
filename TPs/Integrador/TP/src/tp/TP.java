00269105a47591d777850353077004c1793c607a
SilJavJor <104878242+SilJavJor@users.noreply.github.com>
2023-03-13 17:25:09 -0300

    Creacion de Trabajo Practico Argentina Programa

---
+++ TPs/Integrador/TP/src/tp/TP.java
@@ -0,0 +1,19 @@
+/*
+*/
+package tp;
+
+/**
+ *
+ * @author Silvio Jorda
+ */
+
+public class TP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
