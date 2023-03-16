2d6a6e59063292ab3d1691c409f51760def31dc1
15natalia <silvio.jorda@gmail.com>
2023-03-15 21:22:25 -0300

    Actualizacion de archivos

--- TPs/Integrador/TP/src/tp/TP.java
+++ TPs/Integrador/TP/src/tp/TP.java
@@ -1,19 +1,41 @@
 /*
-*/
-package tp;
-
-/**
- *
- * @author Silvio Jorda
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package tp;
 
 public class TP {
 
+    public static PronosticoDeportivo PRODE;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        PRODE = new PronosticoDeportivo();
+        PRODE.cargarEquipos();   
+        System.out.println ("=== EQUIPOS ===");
+        System.out.println (PRODE.listarEquipos());
+        
+        System.out.println("*".repeat(40));
+
+        PRODE.cargarParticipantes();   
+        System.out.println ("=== PARTICIPANTES ===");
+        System.out.println (PRODE.listarParticipantes());
+        
+        System.out.println("*".repeat(40));
+
+        PRODE.cargarPartidos();   
+        System.out.println ("=== PARTIDOS ===");
+        System.out.println (PRODE.listarPartidos());
+        
+        System.out.println("*".repeat(40));
+
+        PRODE.cargarPronosticos();   
+        System.out.println ("=== PRONOSTICO ===");
+        System.out.println (PRODE.listarPronosticos());
+        
     }
     
 }
