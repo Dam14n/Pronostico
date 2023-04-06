ebbc0edcd3d0f19712ce3e11ace263ea145ee6da
SilJavJor <104878242+SilJavJor@users.noreply.github.com>
2023-04-06 15:48:15 -0300

    Se actualizan el TP, el PronosticoDeportivo y ListaEquipos

--- TPs/TP-E2/src/tp/ListaEquipos.java
+++ TPs/TP-E2/src/tp/ListaEquipos.java
@@ -9,38 +9,43 @@ package tp;
  * @author SJorda
  */
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 public class ListaEquipos {
     // Atributos
     private List<Equipo> equipos;
+    private String equiposCSV;
     
     // Metodos
-    public ListaEquipos(List<Equipo> equipos) {
+    public ListaEquipos(List<Equipo> equipos, String equiposCSV) {
         this.equipos = equipos;
+        this.equiposCSV = equiposCSV;
     }
-
+    
     public ListaEquipos() {
         this.equipos = new ArrayList<Equipo>();
+        this.equiposCSV = "equipos.csv";
     }
-
     public List<Equipo> getEquipos() {
         return equipos;
     }
+    
     public void setEquipos(List<Equipo> equipos) {
         this.equipos = equipos;
     }
 
-    // Agregar elementos
-    public void addEquipo(Equipo e) {
-        this.equipos.add(e);
+    // Agregar elemento a la lista
+    public void addEquipo(Equipo equipo) {
+        this.equipos.add(equipo);
     }
 
-    // Eliminar elementos
-    public void removeEquipo(Equipo e) {
-        this.equipos.remove(e);
+    // Eliminar elemento de la lista
+    public void removeEquipo(Equipo equipo) {
+        this.equipos.remove(equipo);
     }
 
     @Override
@@ -52,8 +57,53 @@ public class ListaEquipos {
     public String listar() {
         String lista = "";
         for (Equipo equipo: equipos) {
-            lista += "\n" + equipo;
+            lista += System.lineSeparator() + equipo;  // System.lineSeparator() se utiliza com separador de linea pero por S.O.
         }           
+        
         return lista;
+    }
+
+    // Raaliza la carga desde el archivo especificado
+    public void cargarDeArchivo() {
+        // para las lineas del archivo csv
+        String datosEquipo;
+        // para los datos individuales de cada linea
+        String vectorEquipo[];
+        // para el objeto en memoria
+        Equipo equipo;
+        int fila = 0;
+       
+        try { 
+            Scanner sc = new Scanner(new File("./Equipos.csv"));
+            sc.useDelimiter("\n");   //setea el separador de los datos
+                
+            while (sc.hasNext()) {
+                // levanta los datos de cada linea
+                datosEquipo = sc.next();
+                System.out.println(datosEquipo);  //muestra los datos levantados 
+                fila ++;
+                // si es la cabecera la descarto y no se considera para armar el listado
+                if (fila == 1)
+                    continue;              
+                 
+                //Proceso auxiliar para convertir los string en vector
+                // guarda en un vector los elementos individuales
+                vectorEquipo = datosEquipo.split(",");   
+                
+                // graba el equipo en memoria
+                //convertir un string a un entero;
+                int idEquipo = Integer.parseInt(vectorEquipo[0]);
+                String nombre = vectorEquipo[1];
+                String descripcion = vectorEquipo[2];
+                // crea el objeto en memoria
+                equipo = new Equipo(idEquipo, nombre, descripcion);
+                
+                // llama al metodo add para grabar el equipo en la lista en memoria
+                this.addEquipo(equipo);
+            }
+            //closes the scanner
+        } catch (IOException ex) {
+                System.out.println("Mensaje: " + ex.getMessage());
+        }       
     }
 }
--- TPs/TP-E2/src/tp/TP.java
+++ TPs/TP-E2/src/tp/TP.java
@@ -1,73 +1,19 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ Para entrega 2
  */
 package tp;
-
-import java.io.*;  
-import java.util.Scanner;  
 
 public class TP {
 
     public static PronosticoDeportivo PRODE;
     
-    /**
-     * @param args the command line arguments
-     */
-    
     public static void main(String[] args) {
+        System.out.println ("Sistema de simulación de pronósticos deportivos.");
         
-        String datosEquipo;
-        String vectorEquipo[];
-        Equipo equipo;
-        ListaEquipos lista = new ListaEquipos();
-            //Establece en el costructor de Scanner que va a tomar datos del archivo Equipos.csv
-        try { 
-            Scanner sc = new Scanner(new File("./Equipos.csv"));
-            sc.useDelimiter("\n");   //setea el separador de los datos
-                
-            while (sc.hasNext()) {
-                // levanta los datos de cada linea
-                datosEquipo = sc.next();
-                System.out.println(datosEquipo);  //muestra los datos levantados 
-                // guarda en un vector los elementos
-                vectorEquipo = datosEquipo.split(",");   
-                System.out.println("Nombre: "+vectorEquipo[0]+"Descripcion: "+vectorEquipo[1]);
-                // graba el equipo en memoria
-                equipo = new Equipo (vectorEquipo.hashCode(), vectorEquipo[0], vectorEquipo[1]);
-                // graba el equipo en la lista en memoria
-                lista.addEquipo(equipo);
-            }
-            //closes the scanner
-        } catch (IOException ex) {
-                System.out.println("Mensaje: " + ex.getMessage());
-        }       
-        System.out.println(lista.listar());  //muestra los datos de la lista
-        
-        /*
         PRODE = new PronosticoDeportivo();
-        PRODE.cargarEquipos();   
-        System.out.println ("=== EQUIPOS ===");
-        System.out.println (PRODE.listarEquipos());
-        
-        System.out.println("*".repeat(40));
 
-        PRODE.cargarParticipantes();   
-        System.out.println ("=== PARTICIPANTES ===");
-        System.out.println (PRODE.listarParticipantes());
-        
-        System.out.println("*".repeat(40));
+        PRODE.play();
 
-        PRODE.cargarPartidos();   
-        System.out.println ("=== PARTIDOS ===");
-        System.out.println (PRODE.listarPartidos());
-        
-        System.out.println("*".repeat(40));
-
-        PRODE.cargarPronosticos();   
-        System.out.println ("=== PRONOSTICO ===");
-        System.out.println (PRODE.listarPronosticos());
-        */
     }
     
 }
