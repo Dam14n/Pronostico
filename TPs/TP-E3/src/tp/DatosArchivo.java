af7e9e425824e46014feb35b455e13de64165d78
SilJavJor <104878242+SilJavJor@users.noreply.github.com>
2023-04-12 15:45:48 -0300

    Creacion de Datos de Archivos para ver de una lectura unica

---
+++ TPs/TP-E3/src/tp/DatosArchivo.java
@@ -0,0 +1,14 @@
+/*
+*/
+package tp;
+
+/**
+ *
+ * @author SJorda
+ */
+public class DatosArchivo {
+    private String directotioArchivos;
+    private String nombreArchivo;
+    private String extensionArchivo;
+    private String archivoDatos;    
+}
