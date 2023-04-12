58671731d015dba691117475e1d3e292c9446e73
SilJavJor <104878242+SilJavJor@users.noreply.github.com>
2023-04-12 15:58:35 -0300

    Actualizacion de Datos Archivo

--- TPs/TP-E3/src/tp/DatosArchivo.java
+++ TPs/TP-E3/src/tp/DatosArchivo.java
@@ -7,8 +7,56 @@ package tp;
  * @author SJorda
  */
 public class DatosArchivo {
-    private String directotioArchivos;
-    private String nombreArchivo;
-    private String extensionArchivo;
-    private String archivoDatos;    
+    private String nombreArchivo;   // el nombre deberia ser obligatorio
+    private String extensionArchivo;    // la extension deberia ser abligatoria
+    private String directotioArchivos;  // el directorio de los archivos es el que se indique y se pas deberia ser el directorio don se est ejecutando + Archivos ej (./Archivos/)
+    private String archivoDatos;
+
+    public DatosArchivo(String directotioArchivos, String nombreArchivo, String extensionArchivo, String archivoDatos) {
+        this.directotioArchivos = directotioArchivos;
+        this.nombreArchivo = nombreArchivo;
+        this.extensionArchivo = extensionArchivo;
+        this.archivoDatos = archivoDatos;
+    }
+
+    public DatosArchivo() {
+        this.directotioArchivos = "./Archivos/";
+        this.nombreArchivo = null;
+        this.extensionArchivo = null;
+        this.archivoDatos = null;
+    }
+
+    public String getDirectotioArchivos() {
+        return directotioArchivos;
+        // este deberia ser el valor por defecto System.getProperty("user.dir")
+    }
+
+    public void setDirectotioArchivos(String directotioArchivos) {
+        this.directotioArchivos = directotioArchivos;
+        // el directorio de los archivos deberia ser ./Archivos por defecto
+    }
+
+    public String getNombreArchivo() {
+        return nombreArchivo;
+    }
+
+    public void setNombreArchivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    public String getExtensionArchivo() {
+        return extensionArchivo;
+    }
+
+    public void setExtensionArchivo(String extensionArchivo) {
+        this.extensionArchivo = extensionArchivo;
+    }
+
+    public String getArchivoDatos() {
+        return archivoDatos;
+    }
+
+    public void setArchivoDatos(String archivoDatos) {
+        this.archivoDatos = archivoDatos;
+    }  
 }
