7eab02099174435fb51a9f72905d72aabe5e7e20
SilJavJor <silvio.jorda@gmail.com>
2023-04-13 00:08:48 -0300

    Creacion de TP-3 en Maven

---
+++ Maven/TPs/MiTpMaven/src/main/java/com/mycompany/mitpmaven/MiTpMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mitpmaven;
+
+/**
+ *
+ * @author DIALER
+ */
+public class MiTpMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
