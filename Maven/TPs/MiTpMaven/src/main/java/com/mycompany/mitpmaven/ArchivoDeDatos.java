6e86dc9553d57f7e81a983961c143d70ded313c3
SilJavJor <silvio.jorda@gmail.com>
2023-04-16 00:59:32 -0300

    Actualizacion de Archivo de datos

--- Maven/TPs/MiTpMaven/src/main/java/com/mycompany/mitpmaven/ArchivoDeDatos.java
+++ Maven/TPs/MiTpMaven/src/main/java/com/mycompany/mitpmaven/ArchivoDeDatos.java
@@ -6,7 +6,66 @@ package com.mycompany.mitpmaven;
 * @author  GRUPO 7
 */
 
+import java.io.File;
+import java.io.IOException;
+//import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ArchivoDeDatos {
+    //
+/*
+    private Scanner scanner;
+
+    // Función para abrir el archivo
+    public void openFile(String fileName) {
+        try {
+            scanner = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: el archivo no pudo ser encontrado.");
+        }
+    }
+
+    // Función para cerrar el archivo
+    public void closeFile() {
+        scanner.close();
+    }
+}
+*/
+    
+    //
+    private File file;
+    private Scanner scanner;
+
+    public ArchivoDeDatos(String nombreArchivo) throws IOException {
+        this.file = new File(nombreArchivo);
+        
+        this.scanner = new Scanner(file);
+    }
+
+    public Scanner abrirArchivo(String nombreArchivo) throws IOException {
+        this.file = new File(nombreArchivo);
+        
+        this.scanner = new Scanner(file);
+        return null;
+    }
+
+    // Cierra el archivo abierto
+    public void cerrarArchivo() throws IOException {
+        this.scanner.close();
+    }
+    
+    // 
+    public boolean hayMasLineas() {
+        return this.scanner.hasNextLine();
+    }
+
+    // Devuelve la proxima linea leida
+    public String leerLinea() {
+        return this.scanner.nextLine();
+    }
+}
+
+/*
     private String nombreArchivo;   // el nombre deberia ser obligatorio
     private String extensionArchivo;    // la extension deberia ser abligatoria
     private String directotioArchivos;  // el directorio de los archivos es el que se indique y se pas deberia ser el directorio don se est ejecutando + Archivos ej (./Archivos/)
@@ -60,3 +119,4 @@ public class ArchivoDeDatos {
         this.archivoDatos = archivoDatos;
     }  
 }
+*/
